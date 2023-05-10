efd248c375159f29bb2fac49bcf4f7132146efaa
Tsukamoto Yuto <yuto.tsukamoto@ncdc.co.jp>
2023-05-10 16:04:48 +0900

    fix: android build通るように設定調整

---
+++ android/app/src/main/java/com/example/imageclassification/MainActivity.java
@@ -0,0 +1,9 @@
+package com.example.imageclassification;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.android.FlutterActivity;
+
+
+public class MainActivity extends FlutterActivity {
+}
